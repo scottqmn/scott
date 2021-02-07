c9c99b997aab100806792f77626fae449e39ac75
Scott Nguyen <scottqmn@gmail.com>
2021-02-06 16:35:03 -0800

    Animate grid items

--- components/ItemGrid.jsx
+++ components/ItemGrid.jsx
@@ -1,23 +1,42 @@
 import PropTypes from 'prop-types'
 import clsx from 'clsx'
+import { motion } from 'framer-motion'
 import ProjectItem from './ProjectItem'
 import projectPropTypes from '../prop-types/project'
 import styles from '../styles/components/ItemGrid.module.scss'
 
 const ItemGrid = ({ heading, items }) => {
+    const grid = {
+        hidden: {},
+        visible: {
+            transition: {
+                staggerChildren: 0.1,
+            },
+        },
+    }
+
+    const item = {
+        hidden: { opacity: 0, scale: 0.9 },
+        visible: { opacity: 1, scale: 1, transition: { duration: 0.3 } },
+    }
     return (
         <div className='outer'>
             <div className='inner'>
                 {heading && (
                     <h2 className={clsx(styles.heading, 't-h2')}>{heading}</h2>
                 )}
-                <ul className={styles.grid}>
+                <motion.ul
+                    className={styles.grid}
+                    initial='hidden'
+                    animate='visible'
+                    variants={grid}
+                >
                     {items.map(({ uid, data }) => (
-                        <li key={uid}>
+                        <motion.li key={uid} variants={item}>
                             <ProjectItem uid={uid} data={data} />
-                        </li>
+                        </motion.li>
                     ))}
-                </ul>
+                </motion.ul>
             </div>
         </div>
     )
