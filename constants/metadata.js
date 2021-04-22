323c054b2327e8e65fddbe7449e839d4cbb9d240
Scott Nguyen <scottqmn@gmail.com>
2021-04-22 11:11:00 -0700

    Metadata

--- constants/metadata.js
+++ constants/metadata.js
@@ -1,6 +1,7 @@
 export default {
   title: 'Hey Scott',
-  description: "I'm Scott",
+  description:
+    "I'm Scott Nguyen and I make websites almost as fast as I purchase domain names.",
   image: '/meta_image.jpg',
   favicon: '/favicon.ico',
   url: 'https://heyscott.com',
