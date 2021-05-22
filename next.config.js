94e741d6324d56524b04db2d9bfd6721d7f4c1c5
Scott Nguyen <scottqmn@gmail.com>
2021-05-22 10:45:00 -0700

    redirect

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/qr',
+        destination: '/another',
+        permanent: false,
+      },
+    ]
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
+      use: ['@svgr/webpack'],
+    })
 
-    return config;
+    return config
   },
-};
+}
---
+++ pages/another.tsx
@@ -0,0 +1,6 @@
+import Splash from '../components/Splash'
+
+const Another: React.FC = () => {
+  return <Splash />
+}
+export default Another
