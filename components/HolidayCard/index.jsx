348810ecabcdb6f749bf03d062da60d8bee615f4
Scott Nguyen <scottqmn@gmail.com>
2021-12-24 13:31:00 -0800

    Fonts, card layout

--- components/HolidayCard/index.jsx
+++ components/HolidayCard/index.jsx
@@ -1,26 +1,76 @@
-import { useState, useMemo } from 'react'
-import PropTypes from 'prop-types'
+import { useState } from 'react'
 import clsx from 'clsx'
 import Tilt from 'react-parallax-tilt'
+import IconButton from '@mui/material/IconButton'
+import RefreshIcon from '@mui/icons-material/Refresh'
 import styles from './styles.module.scss'
+
+const MAX_ANGLE = 5
 
 const HolidayCard = () => {
   const [flipped, setFlipped] = useState(false)
+  const toggleFlipped = () => {
+    setFlipped((curr) => !curr)
+  }
 
   return (
-    <div className={styles.container}>
-      <div className={styles.outer}>
-        <div className={clsx(styles.inner, styles.absoluteFill)}>
-          Happy Holidays!
-        </div>
-        <video
-          className={clsx(styles.video, styles.absoluteFill)}
-          src='assets/kona-tree.mp4'
-          autoPlay
-          loop
-          muted
-          playsInline
-        />
+    <div className={styles.outer}>
+      <div className={styles.inner}>
+        <Tilt
+          className={styles.cardOuter}
+          tiltMaxAngleX={MAX_ANGLE}
+          tiltMaxAngleY={MAX_ANGLE}
+        >
+          <div
+            className={clsx(
+              styles.cardInner,
+              styles.absoluteFill,
+              styles.opacityTransition
+            )}
+            style={{ opacity: flipped ? 0 : 1 }}
+            aria-hidden={flipped}
+          >
+            {/* Video background */}
+            <video
+              className={clsx(styles.video, styles.absoluteFill)}
+              src='assets/kona-tree.mp4'
+              autoPlay
+              loop
+              muted
+              playsInline
+            />
+            <h1 className={styles.heading}>Happy Holidays!</h1>
+          </div>
+          {/* Heading */}
+          <div
+            className={clsx(styles.cardInner, styles.opacityTransition)}
+            style={{ opacity: flipped ? 1 : 0 }}
+            aria-hidden={!flipped}
+          >
+            <div className={styles.cardContent}>
+              <p>
+                I made this website so I wouldn't have to buy multiple cards.
+              </p>
+
+              <p style={{ textAlign: 'right' }}>
+                Sincerely,
+                <br />
+                <span>Scott</span>
+              </p>
+            </div>
+          </div>
+          {/* Content */}
+        </Tilt>
+
+        <IconButton
+          type='button'
+          onClick={toggleFlipped}
+          aria-label='Flip card'
+          className={styles.button}
+          color='inherit'
+        >
+          <RefreshIcon />
+        </IconButton>
       </div>
     </div>
   )
--- pages/_document.js
+++ pages/_document.js
@@ -17,10 +17,12 @@ export default class MyDocument extends Document {
       <Html lang='en'>
         <Head>
           {/* Google Fonts */}
-          {/* <link
-            href='https://fonts.googleapis.com/css2?family=Lato&family=Noto+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap'
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Amatic+SC:wght@700&family=Playfair+Display:ital,wght@0,400;0,600;1,400;1,600&display=swap'
             rel='stylesheet'
-          /> */}
+          />
           {/* Google Analytics */}
           {isProduction && (
             <>
